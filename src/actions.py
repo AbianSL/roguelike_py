c25212b5a4ed92444249073e7961cd949d4fa55b
AbianSL <abisanle@gmail.com>
2023-09-05 02:11:12 +0100

    copy the entry lines

--- src/actions.py
+++ src/actions.py
@@ -1,8 +1,25 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
 class Action:
-    pass
+    def perfom(self, engine: Engine, entity: Entity) -> None:
+        '''Perfom this action with the objects needed to determine its scope.
+        
+        'engine' is the scope this action is being perfomed in.
+
+        'entity' is the object perfoming the action.
+
+        This method must be overridden by Action subclasses'''
+        raise NotImplementedError()
 
 class EscapeAction(Action):
-    pass
+    def perfom(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
 
 class MovementAction(Action):
     def __init__(self, dx: int, dy: int):
